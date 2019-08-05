b08cf7dff9dcb9db4a240d42c8290bd73f21bf3c
Louis kane <lkane02@yahoo.com>
2019-08-04 19:02:57 -0700

    adding area component

--- converter/src/App.js
+++ converter/src/App.js
@@ -6,13 +6,14 @@ import Landing from "./components/layout/Landing";
 import Length from "./components/Length";
 import Temperature from "./components/Temperature";
 import Volume from "./components/Volume";
+import Area from "./components/Area";
 import Weight from "./components/Weight";
 
 // import "./App.css";
 // import "./styles/length.css";
 // import "./styles/landing.css";
 import "./styles/app.css";
-import { Row, Col } from "react-bootstrap";
+//import { Row, Col } from "react-bootstrap";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route exact path="/length" component={Length} />
           <Route exact path="/Temperature" component={Temperature} />
           <Route exact path="/volume" component={Volume} />
+          <Route exact path="/Area" component={Area} />
           <Route exact path="/weight" component={Weight} />
         </Switch>
         {/* </Row> */}
